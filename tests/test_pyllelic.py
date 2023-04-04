6a20e8939ea90ae7317128aeed6afbe93761dcc4
Paradoxdruid <bonham@gmail.com>
2023-04-04 15:15:23 -0600

    tests: test sign_meth_diffs

--- tests/test_pyllelic.py
+++ tests/test_pyllelic.py
@@ -641,21 +641,23 @@ class Test_GenomicPositionData:
         pd.testing.assert_frame_equal(EXPECTED_ALLELIC_DATA, actual1)
         pd.testing.assert_frame_equal(EXPECTED_ALLELIC_DATA, actual2)
 
-    # def test_sig_methylation_differences(self, set_up_genomic_position_data, mocker):
-    #     _, genomic_position_data = set_up_genomic_position_data
-    #     mocked_go = mocker.patch("pyllelic.visualization.go")
-    #     mocked_mpl = mocker.patch("pyllelic.visualization.pd.DataFrame.plot")
+    def test_sig_methylation_differences(
+        self, set_up_genomic_position_data: PositionDataTuple, mocker: MockerFixture
+    ) -> None:
+        _, genomic_position_data = set_up_genomic_position_data
+        mocked_go = mocker.patch("pyllelic.visualization.go")
+        mocked_mpl = mocker.patch("pyllelic.visualization.pd.DataFrame.plot")
 
-    #     with np.testing.suppress_warnings() as sup:  # ignore deg of freedom warning
-    #         sup.filter(RuntimeWarning, "Degrees of freedom")
-    #         sup.filter(module=np.ma.core)
-    #         genomic_position_data.sig_methylation_differences()
-    #         genomic_position_data.sig_methylation_differences(backend="plotly")
-    #         genomic_position_data.sig_methylation_differences(backend="matplotlib")
+        with np.testing.suppress_warnings() as sup:  # ignore deg of freedom warning
+            sup.filter(RuntimeWarning, "Degrees of freedom")
+            sup.filter(module=np.ma.core)
+            genomic_position_data.sig_methylation_differences()
+            genomic_position_data.sig_methylation_differences(backend="plotly")
+            genomic_position_data.sig_methylation_differences(backend="matplotlib")
 
-    #         mocked_go.Figure.assert_called()
-    #         mocked_go.Bar.assert_called()
-    #         mocked_mpl.assert_called_once()
+            mocked_go.Figure.assert_called()
+            mocked_go.Bar.assert_called()
+            mocked_mpl.assert_called_once()
 
     def test_sig_methylation_differences_invalid(
         self, set_up_genomic_position_data: PositionDataTuple
